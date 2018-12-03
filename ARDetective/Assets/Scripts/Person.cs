0452b4ee310fc1ffdc920361d22feffba0e9c3b2
KohaiKhaos <bronykhaos@gmail.com>
2018-12-03 12:51:22 -0600

    Starting giving out clue descriptions and redid lots of intro stuff (#4)
    I am a wizard

--- ARDetective/Assets/Scripts/Person.cs
+++ ARDetective/Assets/Scripts/Person.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 
-public class Person{
-  public string firstName;
-  public string lastName;
-  
-  public Person(string fName, string lName){
-      firstName = fName;
-      lastName = lName;
-  }
+public class Person {
+	public string firstName;
+	public string lastName;
+
+	public Person(string fName, string lName) {
+		firstName = fName;
+		lastName = lName;
+	}
+
+	public string fullName  {
+		get { return firstName + " " + lastName; }
+	}
 }
