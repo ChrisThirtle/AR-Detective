9ff61f7065b706478758d9af43f91b11eb905cdb
reiddb <reiddelbello@gmail.com>
2018-12-03 13:48:36 -0600

    Lots of stuff - added vuforia back and scaled stuff

--- ARDetective/Assets/Scripts/ClueHider.cs
+++ ARDetective/Assets/Scripts/ClueHider.cs
@@ -1,65 +1,115 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using GoogleARCore;
+using Vuforia;
 
-public class ClueHider : MonoBehaviour {
+public class ClueHider : MonoBehaviour, ITrackableEventHandler{
 	static System.Random rand = new System.Random();
 
 	bool cluesPlaced = false;
-	List<DetectedPlane> planes = new List<DetectedPlane>();
-	// Use this for initialization
+
 	List<GameObject> hidingObjects = new List<GameObject>();
 	List<GameObject> clueObjects = new List<GameObject>();
 
-	void Start () {
-		hidingObjects.AddRange(Resources.LoadAll<GameObject>("HidingPlacePrefabs/"));
-		clueObjects.AddRange(Resources.LoadAll<GameObject>("CluePrefabs/"));
-	}
-
 	
 	
-
+    private int hidingplacesPlaced;
 	// Update is called once per frame
-	void Update () {
-		if (!cluesPlaced)
-		{
-			Session.GetTrackables<DetectedPlane>(planes);
-			//Remove any subplanes within the list
-			foreach (DetectedPlane plane in planes)
-			{
-				if (null != plane.SubsumedBy)
-					planes.Remove(plane);
-			}
-			//Only do more work if there's at least 4 planes left.
-			if (planes.Count >= 4)
-			{
-				int index = rand.Next(planes.Count-1);
+	public void placeContent()
+    {
+        if (hidingplacesPlaced < 4)
+        {
+            GameObject randHidingPlace = GameObject.Instantiate(hidingObjects[rand.Next(hidingObjects.Count - 1)]);
+            HidingPlace rhp = randHidingPlace.AddComponent<HidingPlace>();
+            GameObject clueMdl = Instantiate(clueObjects[rand.Next(clueObjects.Count - 1)] );
+            rhp.clueModel = clueMdl.GetComponent<Clue>();
+            clueMdl.transform.SetParent(randHidingPlace.transform);
+            clueMdl.SetActive(false);
 
-				//Find a random point on the plane
-				List<Vector3> polygon = new List<Vector3>();
-				float x = planes[index].ExtentX*2 / (float)rand.Next(1,int.MaxValue) - planes[index].ExtentX;
-				float z = planes[index].ExtentZ * 2 / (float)rand.Next(1, int.MaxValue) - planes[index].ExtentZ;
+            GameObject planeClone = GameObject.Instantiate(this.gameObject);
+            randHidingPlace.transform.SetParent(planeClone.transform);
 
-				//Create a position and rotation so a pose can be made
-				Vector3 position = new Vector3(x,0,z);
-				Quaternion rotation = new Quaternion(0,0,0,0);
-				Pose pose = new Pose(position, rotation);
-
-				//Create anchor with pose
-				Anchor anchor = planes[index].CreateAnchor(pose);
+            randHidingPlace.transform.localPosition = new Vector3(0f,-1f,0f);
+            randHidingPlace.transform.localRotation = Quaternion.identity;
+            hidingplacesPlaced++;
+        }
+    }
 
 
-                //TODO: fill out 'HidingPlace' and 'Clue' components' properties
-                //Instantiate and place a HidingPlace
-                GameObject randHidingPlace = GameObject.Instantiate(hidingObjects[rand.Next(hidingObjects.Count-1)] );
-                HidingPlace rhp = randHidingPlace.AddComponent<HidingPlace>();
-                GameObject clueMdl = clueObjects[rand.Next(clueObjects.Count - 1)];
-                rhp.clueModel = clueMdl.AddComponent<Clue>();
-                randHidingPlace.transform.SetParent(anchor.transform,false);
+    protected TrackableBehaviour mTrackableBehaviour;
+    protected TrackableBehaviour.Status m_PreviousStatus;
+    protected TrackableBehaviour.Status m_NewStatus;
 
-				cluesPlaced = true;
-			}
-		}
-	}
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+
+        hidingObjects.AddRange(Resources.LoadAll<GameObject>("HidingPlacePrefabs/"));
+        clueObjects.AddRange(Resources.LoadAll<GameObject>("CluePrefabs/"));
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+        switch (newStatus)
+        {
+            case TrackableBehaviour.Status.DETECTED:
+                goto case TrackableBehaviour.Status.TRACKED;
+            case TrackableBehaviour.Status.TRACKED:
+                goto case TrackableBehaviour.Status.EXTENDED_TRACKED;
+            case TrackableBehaviour.Status.EXTENDED_TRACKED:
+                OnTrackingFound();
+                break;
+            default:
+                if (previousStatus == TrackableBehaviour.Status.TRACKED)
+                    OnTrackingLost();
+                break;
+        }
+
+    }
+
+
+    protected virtual void OnTrackingFound()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = true;
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = true;
+
+        // Enable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = true;
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        print("tracking lost");
+    }
+
 }
--- ARDetective/Assets/Scripts/HidingPlace.cs
+++ ARDetective/Assets/Scripts/HidingPlace.cs
@@ -28,8 +28,9 @@ public class HidingPlace : MonoBehaviour {
             {
                 hit.collider.enabled = false;
                 // transform hiding place into object item
-                GameObject hiding_object = (GameObject)Instantiate(clueModel.gameObject, this.transform.position, this.transform.rotation);
-                this.gameObject.SetActive(false);
+                this.transform.GetChild(0).gameObject.SetActive(true);
+                this.transform.DetachChildren();
+                Destroy(this.gameObject);
             }
         }
     }
